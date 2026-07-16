82326b43fc414bef3f0a6e74bfc4bd8e17d5d5e4
kubawoo <k-wach@o2.pl>
2026-07-16 11:59:59 +0200

    Remove qtcreator config

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,4 +1,4 @@
-option(CDF_BUILD_DB_SQLITE  "Build all DB modules"           ON)
+option(CDF_BUILD_DB_SQLITE  "Build SQLITE DB module"         ON)
 option(CDF_BUILD_EXAMPLES   "Build example programs"         ON)
 option(CDF_BUILD_STATIC     "Build static library in addition to shared" OFF)
 include(CTest)
